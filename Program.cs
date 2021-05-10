b402fe0073c0485c148620ad5537da99704719a4
DeepakBandhu <78710411+DeepakBandhu@users.noreply.github.com>
2021-05-10 03:02:35 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,33 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DailyReport
+namespace MathOperations
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("The Tech Academy.\r\nStudent Daily Report");
-            //user name(string)
-            Console.WriteLine("What is your name?");
-            string userName = Console.ReadLine();
-            //what course are they on(string)
-            Console.WriteLine("What course are you on?");
-            string courseName = Console.ReadLine();
-            //page number(int)
-            Console.WriteLine("What page number are you on?");
-            int pageNumber = Convert.ToInt32(Console.ReadLine());
-            //any help(string)
-            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\"");
-            bool anyHelp = Convert.ToBoolean(Console.ReadLine());
-            //feedback(string)
-            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
-            string anyExperience = Console.ReadLine();
-            //hours of study today(int)
-            Console.WriteLine("How many hours did you study today?");
-            int studyHours = Convert.ToInt32(Console.ReadLine());
-            //Outro Message
-            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.WriteLine("Person 1(Please Enter the Corresponding Information:");
+            Console.WriteLine("Hourly Rate");
+            decimal personOneRate = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Hours Worked Per Week");
+            int personOneHours = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Person 2(Please Enter the Corresponding Information:");
+            Console.WriteLine("Hourly Rate");
+            decimal personTwoRate = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Hours Worked Per Week");
+            int personTwoHours = Convert.ToInt32(Console.ReadLine());
+
+            int oneSalary = personOneHours * (Int32)personOneRate;
+            //52 Working weeks in a year
+            int oneFinalSalary = oneSalary * 52;
+            Console.WriteLine("Person 1's annual salary: " + oneFinalSalary);
+
+            int twoSalary = personTwoHours * (Int32)personTwoRate;
+            int twoFinalSalary = twoSalary * 52;
+            Console.WriteLine("Person 2's annual salary: " + twoFinalSalary);
+
+            bool greaterSalary = oneFinalSalary > twoFinalSalary;
+            Console.WriteLine("Does Person 1 make more money than Person 2?\r\n" + greaterSalary);
             Console.ReadLine();
         }
     }
