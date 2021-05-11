a763913fdd497b3147e00b06c4cb303b6b2f870a
DeepakBandhu <78710411+DeepakBandhu@users.noreply.github.com>
2021-05-10 23:21:30 -0700

    Add files via upload

---
+++ ApprovalProgram/ApprovalProgram/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApprovalProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int personAge = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI? Please answer true or false.");
+            bool personDUI = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int personTickets = Convert.ToInt32(Console.ReadLine());
+
+            //this code will see if all of inputs make the person eligible or not
+            bool ifEligible = personAge > 15 && personDUI == false && personTickets <= 3;
+
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(ifEligible);
+            Console.ReadLine();
+        }
+    }
+}
