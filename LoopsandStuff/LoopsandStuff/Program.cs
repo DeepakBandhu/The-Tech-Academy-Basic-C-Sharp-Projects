0b36043702c231524df8e75e2e9e5f8b40eeadad
DeepakBandhu <78710411+DeepakBandhu@users.noreply.github.com>
2021-05-13 23:06:37 -0700

    Add files via upload

---
+++ LoopsandStuff/LoopsandStuff/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopsandStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess my age.");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            bool ageGuessed = age == 21;
+
+            do
+            {
+                switch (age)
+                {
+                    case 62:
+                        Console.WriteLine("You guessed 62. Too high, try again.");
+                        Console.WriteLine("Guess my age");
+                        age = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 29:
+                        Console.WriteLine("You guessed 29. Very close, stay in the 20's, try again.");
+                        Console.WriteLine("Guess my age");
+                        age = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 21:
+                        Console.WriteLine("Good job, I am indeed 21 years old.");
+                        ageGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong.");
+                        Console.WriteLine("Guess my age");
+                        age = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (!ageGuessed);
+
+
+
+            Console.WriteLine("A simple while loop.");
+            {
+                int i = 0;
+                while (i < 5)
+                {
+                    Console.WriteLine(i);
+                    i++;
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
