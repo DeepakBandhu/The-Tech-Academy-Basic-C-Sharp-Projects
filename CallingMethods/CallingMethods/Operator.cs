dccfbb82fe55ca7a48d48688ee1247a39dafd1a9
DeepakBandhu <78710411+DeepakBandhu@users.noreply.github.com>
2021-05-15 05:03:17 -0700

    Add files via upload

---
+++ CallingMethods/CallingMethods/Operator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public class Operator
+    {
+        //using double allows the Get functions ot not be classes but im not sure if that is correct
+        public double GetAdd(int data)
+        {
+            data = data + 3;
+            return (data);
+        }
+
+        public double GetSubtract(double data)
+        {
+            data = data - 3;
+            return (data);
+        }
+
+        public double GetDivide(double data)
+        {
+            data = data / 3;
+            return (data);
+        }
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        public static int Data { get; set; }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter input:");
+
+            string line = Console.ReadLine();
+            Data = Int32.Parse(line);
+
+            var operatorObject = new Operator();
+            //call the 3 methods to produce the results
+            var result = operatorObject.GetAdd(Data);
+            var result2 = operatorObject.GetDivide(Data);
+            var result3 = operatorObject.GetSubtract(Data);
+            Console.WriteLine("Here is result 1" + result + "\nHere is result 2" + result2 + "\nand finally result 3" + result3);
+
+
+            Console.ReadLine();
+        }
+    }
+}
