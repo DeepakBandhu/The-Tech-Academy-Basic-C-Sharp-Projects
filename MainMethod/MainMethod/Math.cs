52d440bdefc9b7ea45d4a8bb2aaadc3de5405298
DeepakBandhu <78710411+DeepakBandhu@users.noreply.github.com>
2021-05-18 10:14:10 -0700

    Add files via upload

---
+++ MainMethod/MainMethod/Math.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    public class Math
+    {
+        public int MathOperation(int data)
+        {
+            int newData = data + 5;
+            return (newData);
+        }
+
+        public decimal MathOperation(decimal data)
+        {
+            decimal newData = 20.5m - data;
+            return (newData);
+        }
+
+        public static string MathOperation(string data)
+        {
+            Console.WriteLine("Enter a number");
+            data = Console.ReadLine();
+            int newData = Convert.ToInt32(data);
+            return newData;
+        }
+    }
+}
---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        public static int Data { get; set; }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter Input: ");
+
+                string line = Console.ReadLine();
+                Data = Int32.Parse(line);
+
+                var mathObjects = new Math();
+                var result = mathObjects.MathOperation(Data);
+                var result2 = mathObjects.MathOperation(Data);
+
+                Console.WriteLine("Here is the result: " + result2);
+
+                Console.ReadLine();
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Please enter a whole number");
+                Console.ReadLine();
+            }
+        }
+    }
+}
