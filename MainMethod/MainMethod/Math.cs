8d16c6c1958e72bafe03c8cb0774a4953db58ad5
DeepakBandhu <78710411+DeepakBandhu@users.noreply.github.com>
2021-05-19 11:02:12 -0700

    Add files via upload

---
+++ MainMethod/MainMethod/Math.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    public class Math
+    {
+        public int MathOperation(int data)
+        {//int input
+            int newData = data + 5;
+            return (newData);
+        }
+
+        public decimal MathOperation(decimal data)
+        {//decimal input
+            int newData = decimal.ToInt32(data);
+            int result = (int)(newData + 2.2m);
+            return (result);
+        }
+
+        public string MathOperation(string data)
+        {//string input
+            int newData = int.Parse(data);
+            int result = newData - 2;
+            return result.ToString();
+           
+        }
+
+    }
+}
---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        public static int Data { get; set; }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter Input: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+
+                //i used my own input because i didnt know how to set up the readline
+                decimal num2 = Convert.ToDecimal(20.0543m);
+
+                Console.WriteLine("Enter 3rd Input: ");
+                string num3 = Console.ReadLine();
+
+                var mathObjects = new Math();
+                var result = mathObjects.MathOperation(num1);
+                var result2 = mathObjects.MathOperation(num2);
+                var result3 = mathObjects.MathOperation(num3);
+
+                Console.WriteLine("First: " + result + "\nSecond: " + result2 + "\nThird: " + result3);
+
+                Console.ReadLine();
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.ReadLine();
+            }
+        }
+    }
+}
