69270661377558755b8c86f19f3d45424f872007
DeepakBandhu <78710411+DeepakBandhu@users.noreply.github.com>
2021-05-19 09:57:29 -0700

    Add files via upload

---
+++ MethodClassSubmission/MethodClassSubmission/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubmission
+{
+    class Program
+    {
+        public static int Data { get; set; }
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 21;
+
+            Parameters n = new Parameters();
+            // i instantiated the object above and now i called a method on the object below.
+            Console.WriteLine("Here is the value that was supoposed to be printed to the screen.");
+            n.twoMaths(a,b);
+            Console.ReadLine();
+          
+        }
+    }
+}
