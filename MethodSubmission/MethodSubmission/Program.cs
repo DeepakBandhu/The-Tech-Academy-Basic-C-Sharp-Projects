71faa30e29631941c3647ee68bc41f97822a9d11
DeepakBandhu <78710411+DeepakBandhu@users.noreply.github.com>
2021-05-19 14:35:10 -0700

    Add files via upload

---
+++ MethodSubmission/MethodSubmission/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission
+{
+    class Program
+    {
+        public static int Data { get; set; }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter a number:");
+                int data1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter another number, OR leave blank:");
+                string data2 = Console.ReadLine();
+
+                var objects = new TwoInts();
+
+                if (data2 == "")
+                {
+                    var result = objects.twoMaths(data1);
+                    Console.WriteLine(result);
+                }
+
+                
+                else
+                {
+                    Convert.ToInt32(data2);
+                    var result2 = objects.twoMaths(data1, data2);//i dont know why this is wrong
+                    Console.WriteLine(result2);
+                }
+
+          
+                Console.ReadLine();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.ReadLine();
+            }
+        }
+    }
+}
