55d273991296f6ec1ce876962a076b162712ee32
DeepakBandhu <78710411+DeepakBandhu@users.noreply.github.com>
2021-05-19 08:35:48 -0700

    Add files via upload

---
+++ MainMethod/MainMethod/Math.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    public class Math
+    {
+        public int MathOperation(int data)
+        {
+            int newData = data + 5;
+            return (newData);
+        }
+
+        public decimal MathOperation(decimal data)
+        {
+            decimal newData = Convert.ToInt32(Console.ReadLine());
+            int newMath = newData - data;
+            return (newMath);
+        }
+
+        internal string MathOperation(string data)
+        {
+            data = Console.ReadLine();
+            int myVal = Convert.ToInt32(data);
+            return data;
+        }
+
+    }
+}
---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        public static int Data { get; set; }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter Input: ");
+
+                string line = Console.ReadLine();
+                int Data = Convert.ToInt32(Console.ReadLine());
+                //Data = Int32.Parse(line);
+
+                var mathObjects = new Math();
+                var result = mathObjects.MathOperation(Data);
+                var result2 = mathObjects.MathOperation(Data);
+                var result3 = mathObjects.MathOperation(Data);
+
+                Console.WriteLine(result);
+
+                Console.ReadLine();
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.ReadLine();
+            }
+        }
+    }
+}
