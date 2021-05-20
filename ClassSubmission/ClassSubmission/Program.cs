f74e8449389df24f9204322c5dd19b58312dcea5
DeepakBandhu <78710411+DeepakBandhu@users.noreply.github.com>
2021-05-19 17:18:06 -0700

    Add files via upload

---
+++ ClassSubmission/ClassSubmission/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operations math = new Operations();
+
+            int a = 50;
+            Console.WriteLine("Before method call, value of a : {0}", a);
+            math.getValue(out a);
+
+            Console.WriteLine("After method call, value of a : {0}", a);
+
+
+            Console.WriteLine("Enter a number to be cut in half:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            math.perform(num1);
+
+
+            Console.WriteLine("And one to be added onto by two: ");
+            string num2 = Console.ReadLine();
+            math.perform(num2);
+
+            Operations instance = new Operations();
+            Console.WriteLine(Operations.StaticMethod());
+           
+
+            Console.ReadLine();
+
+        }
+    }
+}
