4cae55f66d8d92bdecd6da13a05d012f9b957e24
DeepakBandhu <78710411+DeepakBandhu@users.noreply.github.com>
2021-05-20 16:05:09 -0700

    Add files via upload

---
+++ PolymorphismSubmission/PolymorphismSubmission/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismSubmission
+{
+    public abstract class Person // this now has to be used in its inherited class 
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public virtual void SayName()
+        {
+            Console.WriteLine(firstName + " " + lastName);
+        }
+
+    }
+}
---
+++ PolymorphismSubmission/PolymorphismSubmission/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IQuittable object1 = new Employee();
+            //person.
+            Employee employee = new Employee();//instantiation for use
+            employee.firstName = "Deepak";
+            employee.lastName = "Bandhu";
+
+            object1.Quit(employee);
+            Console.WriteLine(employee.firstName + " " + employee.lastName);
+            Console.ReadLine();
+        }
+    }
+}
