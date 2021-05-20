ba709cb527ace0fe289cb1ae9dea34059a5a0b38
DeepakBandhu <78710411+DeepakBandhu@users.noreply.github.com>
2021-05-20 10:46:05 -0700

    Add files via upload

---
+++ MethodsAndObjects/MethodsAndObjects/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Worker = new Person();
+            Worker.FirstName = "Deepak";// giving the properties values, i think thats what i was supposed to do
+            Worker.LastName = "Bandhu";
+
+            Worker.SayName();//after instantiation i can use this method
+            Console.ReadLine();
+        }
+    }
+}
