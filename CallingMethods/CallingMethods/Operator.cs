f30d6f283ec957e584bf56eebc9edbbcbf606e5e
DeepakBandhu <78710411+DeepakBandhu@users.noreply.github.com>
2021-05-20 11:04:36 -0700

    Add files via upload

---
+++ CallingMethods/CallingMethods/Operator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public static class Operator
+    {
+        //using double allows the Get functions ot not be classes but im not sure if that is correct
+        public static double GetAdd(int data)
+        {
+            int newData = data + 3;
+            return (newData);
+        }
+
+        public static double GetSubtract(int data)// making the class static required everything else to be static
+        {
+            int newData = data - 3;
+            return (newData);
+        }
+
+        public static double GetDivide(int data)
+        {
+            int newData = data / 3;
+            return (newData);
+        }
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        public static int Data { get; set; }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter input:");
+
+            int Data = Convert.ToInt32(Console.ReadLine());// changed the input to a readline instead of a parse due to an error
+            //Data = Int32.Parse(line);
+
+            //call the 3 methods to produce the results
+            var result = Operator.GetAdd(Data);
+            var result2 = Operator.GetDivide(Data);//instead of having to instantiate, I was able to just use the class itself
+            var result3 = Operator.GetSubtract(Data);
+            Console.WriteLine("Here is result 1: " + result + "\nHere is result 2: " + result2 + "\nand finally result 3: " + result3);
+
+
+            Console.ReadLine();
+        }
+    }
+}
