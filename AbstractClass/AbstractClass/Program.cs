88fa15b548ad9a1ee31050d2cb78bb6b42f86c5b
DeepakBandhu <78710411+DeepakBandhu@users.noreply.github.com>
2021-05-20 12:22:57 -0700

    Add files via upload

---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.ID = "Joe";
+            //Person person = new Employee();
+            employee.ID = "Deepak";
+
+             bool stuff = employee.ID == employee.ID;
+
+            Console.WriteLine(stuff);
+
+            Console.ReadLine();
+
+            //Employee Worker = new Employee();//instantiated Employee
+            //Worker.firstName = "Deepak";
+            //Worker.lastName = "Bandhu";
+            //Worker.SayName();// this is called from Employee
+
+        }
+    }
+}
