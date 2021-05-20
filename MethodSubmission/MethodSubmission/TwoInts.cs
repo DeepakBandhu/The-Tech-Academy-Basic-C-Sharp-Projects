8edf29e35b1b58067d4aa62e02cfc52b31acb670
DeepakBandhu <78710411+DeepakBandhu@users.noreply.github.com>
2021-05-19 17:17:41 -0700

    Add files via upload

---
+++ MethodSubmission/MethodSubmission/TwoInts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission
+{
+    public class TwoInts
+    {
+        public int twoMaths(int int1, int int2 = 5)
+        {
+            int result = int1 + 5 + int2;
+            return result;
+        }
+    }
+}
