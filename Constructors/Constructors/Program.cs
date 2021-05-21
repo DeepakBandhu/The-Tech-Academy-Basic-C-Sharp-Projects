25cfc288217449f78af72ea499fc2e601d24b735
DeepakBandhu <78710411+DeepakBandhu@users.noreply.github.com>
2021-05-21 02:18:46 -0700

    Add files via upload

---
+++ Constructors/Constructors/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string name = "Deepak";
+
+            var date = DateTime.Now;
+
+            sample stuff = new sample();
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Constructors/Constructors/sample.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    public class sample
+    {
+        public sample() : this(10)
+        {
+            // No parameter constructor method.// First Constructor  
+        }
+
+        public sample(int Age)
+        {
+            // Constructor with one parameter.// Second Constructor}  
+        }
+
+    }
+}
