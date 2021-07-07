50cb30885e4df744c3ce7885c50dade10bda68bf
DeepakBandhu <78710411+DeepakBandhu@users.noreply.github.com>
2021-07-07 09:57:59 -0700

    Add files via upload

---
+++ Constructors/Constructors/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string name = "Bandhu";
+
+            var date = DateTime.Now;
+
+            sample myName = new sample();
+
+            Console.WriteLine("My name is {0}, my last name is {1}, and it is {2}.", myName.personName, name, date );
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Constructors/Constructors/sample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    public class sample
+    {
+        public string personName;
+
+        public sample() : this("Deepak")
+        {
+
+        }
+
+        public sample(string name)
+        {
+            personName = name;
+        }
+    }
+}
